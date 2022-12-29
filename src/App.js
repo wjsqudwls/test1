759f73fac80d1031ae8ffbb02f23349f4f95c252
wjsqudwls <puuy9@naver.com>
2022-12-29 11:35:39 +0900

    d

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+function User(props) {
+  return <div className="title">{props.user.name}</div>;
+}
+
+const App = () => {
+  const [users, setUsers] = useState([{ id: 0, name: "리액트배우자" }]);
+  const [name, setName] = useState("");
+
+  const addUserHandler = () => {
+    const newUser = {
+      id: users.length + 1,
+      name: name,
+    };
+    setUsers([...users.newUser]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="apptitle">
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <button onClick={addUserHandler}>추가하기</button>
+      </div>
+
+      <div>
+        {users.map((user) => {
+          return <User user={user} key={user.id}></User>;
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
